f3c4ed85fe3c8b95dead4cf655155f1325fc2ddc
Keagan <keagananderson2005@gmail.com>
2025-03-11 09:37:07 -0500

    Lisa

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ Spot Finder!
 Project Spot 2025
 
 Add your name below once you get github working \/
-Keagan,Ayman Faruqi
+Keagan,Ayman Faruqi, Lisa
 
 
 ENSURE API keys are never exposed to the front. Keep keys in seperate files and make sure the file is ignored in git. 
