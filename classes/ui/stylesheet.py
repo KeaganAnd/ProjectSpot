72255cbb0ab42d69565ccc6aa3d4647ec7a02ca8
Keagan <keagananderson2005@gmail.com>
2025-03-19 11:08:38 -0500

    f

---
+++ classes/ui/stylesheet.py
@@ -0,0 +1,26 @@
+def returnStyleSheet():
+    return("""
+    [class="header1"] {
+           color: #F5F5F5;
+           font-size: 49px;
+           padding-top: 25px;
+           padding-bottom: 15px;
+           font-family:  Times New Roman;
+           }    
+    QMainWindow
+           {
+           background-color: #37474F;
+           }
+    QLineEdit
+           {
+           background-color: #A5D6A7;
+           color: #F5F5F5;
+           min-height: 50px;
+           max-height: 50px;
+           font-size: 20px;
+           width: 100px;
+           border-radius: 20px;
+
+           }
+    
+""")
