aadc3a748e9ab85c7c0a74e38fd0bdb6346705e4
Keagan <keagananderson2005@gmail.com>
2025-04-09 20:24:38 -0500

    k

---
+++ functions/getPovertyData.py
@@ -0,0 +1,14 @@
+import requests
+
+def getPovertyData():
+    '''https://api.census.gov/data/timeseries/poverty/saipe?get=NAME,SAEMHI_PT,SAEPOVALL_PT&for=state:01&YEAR=2023
+    
+    https://www.census.gov/data/developers/data-sets/Poverty-Statistics.html
+
+    Refrence for variable names:
+    https://api.census.gov/data/timeseries/poverty/saipe/variables.html
+    '''
+
+    stateID = 1
+
+    request = requests.get(f"https://api.census.gov/data/timeseries/poverty/saipe?get=NAME,SAEMHI_PT,SAEPOVALL_PT&for=state:{stateID}&YEAR=2023")
