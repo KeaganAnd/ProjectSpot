48c6deb18e356f5e4beae21d4f983b8be7a1a0cd
Keagan <keagananderson2005@gmail.com>
2025-04-15 09:30:30 -0500

    Commented batch file

--- inspectWindow.bat
+++ inspectWindow.bat
@@ -1,1 +1,3 @@
+:: This script is for windows machines with PyQtInspect. Allows you
+:: to open and inspect the PyQt Windows for debugging.
 sudo python -m PyQtInspect --direct --qt-support=pyqt6  --file main.py 
